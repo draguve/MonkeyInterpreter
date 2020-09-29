fa704d53836775a5795efeb9b63383a59bcce5e3
draguve <draguve@gmail.com>
2020-09-29 22:04:30 +0530

    Init Commit added tokens and lexer

---
+++ lexer/lexer_test.go
@@ -0,0 +1,144 @@
+package lexer
+
+import (
+	"MonkeyInterpreter/token"
+	"testing"
+)
+
+//func TestNextToken(t *testing.T) {
+//	input := `=+(){},;`
+//	tests := []struct {
+//		expectedType token.TType
+//		expectedLiteral string
+//	}{
+//		{token.ASSIGN, "="},
+//		{token.PLUS, "+"},
+//		{token.LBRACK, "("},
+//		{token.RBRACK, ")"},
+//		{token.LBRACE, "{"},
+//		{token.RBRACE, "}"},
+//		{token.COMMA, ","},
+//		{token.SEMICOLON, ";"},
+//		{token.EOF, ""},
+//	}
+//	l := New(input)
+//	for i, tt := range tests {
+//		tok := l.NextToken()
+//		if tok.Type != tt.expectedType {
+//			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+//				i, tt.expectedType, tok.Type)
+//		}
+//		if tok.Literal != tt.expectedLiteral {
+//			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+//				i, tt.expectedLiteral, tok.Literal)
+//		}
+//	}
+//}
+
+func TestNextToken(t *testing.T){
+	input := `let five = 5;
+let ten = 10;
+let add = fn(x, y) {
+x + y;
+};
+let result = add(five, ten);
+!-5;
+5 < 10 > 5;
+
+if(5 < 10){
+	return true;
+} else {
+	return false;
+}
+10 == 10;
+10 != 9;`
+	tests := []struct {
+		expectedType token.TType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LBRACK, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RBRACK, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LBRACK, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RBRACK, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LBRACK, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RBRACK, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
---
+++ token/token.go
@@ -0,0 +1,59 @@
+package token
+
+type TType string
+
+var keywords = map[string]TType{
+	"fn" :  FUNCTION,
+	"let" : LET,
+	"true" : TRUE,
+	"false" : FALSE,
+	"if" : IF,
+	"else" : ELSE,
+	"return" : RETURN,
+}
+
+type Token struct {
+	Type    TType  //this will hold what type the token it is
+	Literal string //this will hold what was in the source code
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	IDENT = "IDENT" // names of variables or functions
+	INT = "INT" // 4,48 etc
+
+	ASSIGN = "="
+	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	LT = "<"
+	GT = ">"
+	EQ = "=="
+	NOT_EQ = "!="
+
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LBRACK = "("
+	RBRACK = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+)
+
+//TokenType Returns the type of Token when ident is supplied with a string
+func WhichTokenType(ident string) TType {
+	if tok,ok := keywords[ident];ok {
+		return tok
+	}
+	return IDENT
+}
