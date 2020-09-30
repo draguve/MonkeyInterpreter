60a42667b730298418cfa0e47cdede1906d74e8b
draguve <draguve@gmail.com>
2020-09-30 14:01:06 +0530

    parsing let statements

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,8 @@
 package ast
 
-import "MonkeyInterpreter/token"
+import (
+	"MonkeyInterpreter/token"
+)
 
 type Node interface {
 	 TokenLiteral() string
@@ -19,7 +21,6 @@ type Expression interface {
 type Program struct {
 	Statements []Statement
 }
-
 func (p *Program) TokenLiteral() string{
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -28,21 +29,25 @@ func (p *Program) TokenLiteral() string{
 	}
 }
 
-type Identifier struct {
-	Token token.Token // the token.IDENT token
-	Value string // this will store the name of the variable/function
-}
-
 type LetStatement struct {
 	Token token.Token //the LET Token
 	Name *Identifier //token for the name of the variable/function
 	Value Expression // the other side of the let
 }
+func (l *LetStatement) TokenLiteral() string{
+	return l.Token.Literal
+}
+func (l *LetStatement) statementNode(){}
 
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string // this will store the name of the variable/function
+}
 func (i *Identifier) expressionNode() {
 
 }
-
 func (i *Identifier) TokenLiteral() string{
 	return i.Token.Literal
 }
+
